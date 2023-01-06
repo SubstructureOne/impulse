a357df16d1cbe098e3bf3defb890a009cecdb7a0
dek <dek@substructure.one>
2023-01-05 22:35:41 -0500

    Remove old balances model

--- src/models.rs
+++ src/models.rs
@@ -10,13 +10,3 @@ use crate::schema::*;
 pub mod reports;
 pub mod charges;
 pub mod transactions;
-
-
-#[derive(Queryable, Debug)]
-pub struct Balance {
-    pub user_id: uuid::Uuid,
-    pub balance: f64,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: DateTime<Utc>,
-}
-
