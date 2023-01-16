0ff7b13ea45899ca1f7e5a34badbe8327c8d4b7a
dek <dek@substructure.one>
2023-01-15 19:16:05 -0500

    Generate transactions from CLI

--- src/bin/impulse.rs
+++ src/bin/impulse.rs
@@ -22,18 +22,24 @@ pub async fn main() -> Result<()> {
     env_logger::init();
     dotenvy::dotenv().ok();
     let args = Args::parse();
+    let db_name = std::env::var("DB_NAME")?;
+    let config = Rc::new(ManagementConfig::from_env()?);
+    let manager = PostgresManager::new(config.clone());
+    let mut conn = manager.pg_connect_db(&db_name)?;
     if args.generate_charges {
         info!("Generating charges");
-        let db_name = std::env::var("DB_NAME")?;
-        let config = Rc::new(ManagementConfig::from_env()?);
-        let manager = PostgresManager::new(config.clone());
-        let mut conn = manager.pg_connect_db(&db_name)?;
         let uncharged = ReportToCharge::uncharged(&mut conn)?;
         let charges = Charge::create_charges(&mut conn, uncharged)?;
+        info!("Generated {} charges", charges.len());
+    }
+    if args.generate_transactions {
+        info!("Generating transactions");
+        let charges = Charge::untransacted(&mut conn)?;
         let transactions = NewTransaction::from_charges(
             &mut conn,
             &charges
-        );
+        )?;
+        info!("Generated {} transactions", transactions.len());
    }
     Ok(())
 }
