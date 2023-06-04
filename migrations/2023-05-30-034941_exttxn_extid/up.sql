085e4bc2642e26bd3eb3069eaf89c2d83be94a4b
dek <dek@substructure.one>
2023-06-03 22:09:16 -0400

    Fix external transaction external id db type

--- migrations/2023-05-30-034941_exttxn_extid/up.sql
+++ migrations/2023-05-30-034941_exttxn_extid/up.sql
@@ -1,5 +1,5 @@
 -- add a unique external reference to external transactions to prevent double-counting
-ALTER TABLE exttransactions ADD COLUMN exttransaction_extid uuid;
-UPDATE exttransactions SET exttransaction_extid = exttransaction_id WHERE TRUE;
+ALTER TABLE exttransactions ADD COLUMN exttransaction_extid TEXT;
+UPDATE exttransactions SET exttransaction_extid = exttransaction_id::text WHERE TRUE;
 ALTER TABLE exttransactions ALTER COLUMN exttransaction_extid SET NOT NULL;
 CREATE UNIQUE INDEX exttransactions_extid_index ON exttransactions (exttransaction_extid);
