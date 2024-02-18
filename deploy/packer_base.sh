2cda736bf912fad7c0cb27b682e7b10497fc8dd6
dek <dek@substructure.one>
2024-02-18 10:30:07 -0500

    Install fluent to base image

--- deploy/packer_base.sh
+++ deploy/packer_base.sh
@@ -2,6 +2,10 @@
 
 set -euxo pipefail
 
+mkdir -p downloads/
+curl -o downloads/fluent-apt-source.deb https://packages.treasuredata.com/lts/5/ubuntu/jammy/pool/contrib/f/fluent-lts-apt-source/fluent-lts-apt-source_2023.7.29-1_all.deb
+
+
 packer build \
     -var-file=variables.hcl \
     "$@" \
